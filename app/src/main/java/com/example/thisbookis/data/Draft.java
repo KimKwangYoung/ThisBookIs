eab9caf8b00cb3dee4bc58e4df90198be34ee735
Kim Kwangyoung <rhkddud9531@gmail.com>
2020-02-28 21:20:32 +0900

    임시 저장을 위한 Draft 클래스 추가
    - 작성해놓은 제목, 내용, 공개여부, 저장 시간, 책 isbn, 책 제목을 저장하는 클래스

---
+++ app/src/main/java/com/example/thisbookis/data/Draft.java
@@ -0,0 +1,65 @@
+package com.example.thisbookis.data;
+
+import androidx.annotation.Keep;
+
+import java.io.Serializable;
+
+@Keep
+public class Draft implements Serializable {
+    String title;
+    String content;
+    Boolean shouldShare;
+    String saveTime;
+    String bookISBN;
+    String bookTitle;
+    String draftKey;
+
+    public Draft(String title, String content, Boolean shouldShare, String saveTime, String bookISBN, String bookTitle) {
+        this.title = title;
+        this.content = content;
+        this.shouldShare = shouldShare;
+        this.saveTime = saveTime;
+        this.bookISBN = bookISBN;
+        this.bookTitle = bookTitle;
+    }
+
+    public String getDraftKey() {
+        return draftKey;
+    }
+
+    public void setDraftKey(String draftKey) {
+        this.draftKey = draftKey;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Boolean getShouldShare() {
+        return shouldShare;
+    }
+
+    public void setShouldShare(Boolean shouldShare) {
+        this.shouldShare = shouldShare;
+    }
+
+    public String getSaveTime() {
+        return saveTime;
+    }
+
+    public void setSaveTime(String saveTime) {
+        this.saveTime = saveTime;
+    }
+}
